fbc5a372e193c09d0eb0cecc128fe722392288de
LRydin <leonardo.rydin@gmail.com>
2020-01-01 18:56:56 +0100

    Adding test and Travis CI yml

---
+++ test/test_MFDFA.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import sys
+sys.path.append("../")
+from MFDFA import MFDFA
+
+def test_MFDFA():
+    for N in [10_000, 1_000_000]:
+        for q_list in [1, 2, 6, 21]:
+            for order in [1,2,3,4]:
+
+                X = np.random.normal(size = N, loc = 0)
+                q = np.linspace(-10, 10, q_list)
+
+                lag = np.unique(
+                      np.logspace(
+                      0, np.log10(X.size // 4), 25
+                      ).astype(int) + 1 + order
+                      )
+
+                lag, dfa = MFDFA(X, lag = lag, q = q, order = 1)
+
+                assert dfa.ndim == 2, "Output is not 2 dimensional"
+                assert dfa.shape[1] <= q.shape[0], "Output shape mismatch"
