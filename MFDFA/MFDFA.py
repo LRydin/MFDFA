1e4008c324c7a3e579577bcefd20f41914e63e2d
LRydin <leonardo.rydin@gmail.com>
2021-01-22 18:30:27 +0100

    `lag` now non-optional variable. Never was...

--- MFDFA/MFDFA.py
+++ MFDFA/MFDFA.py
@@ -9,7 +9,7 @@ import numpy as np
 from numpy.polynomial.polynomial import polyfit, polyval
 from .emddetrender import detrendedtimeseries
 
-def MFDFA(timeseries: np.ndarray, lag: np.ndarray=None, order: int=1,
+def MFDFA(timeseries: np.ndarray, lag: np.ndarray, order: int=1,
           q: np.ndarray=2, stat: bool=False, modified: bool=False,
           extensions: dict={'EMD':False, 'eDFA':False, 'window':False}) -> np.ndarray:
     """
