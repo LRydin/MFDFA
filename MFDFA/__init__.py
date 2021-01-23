f93856ce5958ff1f18c8429a300b228b9428a86a
LRydin <leonardo.rydin@gmail.com>
2021-01-23 17:19:00 +0100

    Added the multifractal exponents tau and a plotter.

--- MFDFA/__init__.py
+++ MFDFA/__init__.py
@@ -1,5 +1,7 @@
 from .MFDFA import MFDFA
 from .fgn import fgn
 from .emddetrender import detrendedtimeseries, IMFs
+from .singspect import singularity_spectrum, singularity_spectrum_plot
+from .singspect import scaling_exponents, scaling_exponents_plot
 
 name = "MFDFA"
--- MFDFA/singspect.py
+++ MFDFA/singspect.py
@@ -9,9 +9,8 @@ import numpy as np
 
 from numpy.polynomial.polynomial import polyfit
 
-import matplotlib.pyplot as plt
-
-def singularity_spectrum(lag, mfdfa_data, q, lim = [None, None]):
+def singularity_spectrum(lag: np.array, mfdfa: np.ndarray, q: np.array,
+                        lim: list=[None, None], interpolate: int=False):
     """
     Extract the slopes of the fluctuation function to futher obtain the
     the singularity strength ``hq`` (or Hölder exponents) and singularity
@@ -19,30 +18,34 @@ def singularity_spectrum(lag, mfdfa_data, q, lim = [None, None]):
 
     Parameters
     ----------
-    lag: np.ndarray of ints
+    lag: np.array of ints
         An array with the window sizes which where used in MFDFA.
 
     mdfda: np.ndarray
         Matrix of the fluctuation function from MFDFA
 
-    q: np.ndarray
+    q: np.array
         Fractal exponents used. Must be more than 2 points.
 
     lim: list (default = [None, None])
         List of lower and upper lag limits. If none, the polynomial fittings
         include the full range of lag.
 
+    interpolate: int (default False)
+        Interpolates the ``q`` space to smoothed the singularity spectrum. Not
+        yet implemented.
+
     Returns
     -------
-    hq: np.ndarray
-        Singularity strength ``hq``. The width of this function indicates the
-        strength of the multifractality. A width of ``max(hq) - min(hq) ≈ 0``
-        means the data is monofractal.
+    tau: np.array
+        Scaling exponents ``tau``. A usually increasing function of ``q`` from
+        which the fractality of the data can be determined by its shape. A truly
+        linear tau indicates monofractality, whereas a curved one (usually
+        curving around small ``q`` values) indicates multifractality.
 
-        Dq: np.ndarray
-        Singularity spectrum ``Dq``. The location of the maximum of ``Dq`` (with
-         ``hq`` as the abscissa) should be 1 and indicates the most prominent
-         exponent in the data.
+    Notes
+    -----
+    .. versionadded:: 0.4.1
 
     References
     ----------
@@ -52,19 +55,8 @@ def singularity_spectrum(lag, mfdfa_data, q, lim = [None, None]):
         316(1-4), 87–114, 2002.
     """
 
-    # Fractal powers as floats
-    q = np.asarray_chkfinite(q, dtype = float)
-
-    slopes = np.zeros(len(q))
-
-    # Find slopes of each q-power
-    for i in range(len(q)):
-        slopes[i] = polyfit(np.log(lag[lim[0]:lim[1]]),
-                            np.log(mfdfa_data[lim[0]:lim[1],i]),
-                            1)[1]
-
     # Calculate tau
-    tau = _tau(slopes, q)
+    tau = scaling_exponents(lag, mfdfa, q, lim, interpolate)
 
     # Calculate hq, which needs tau
     hq = _hq(tau, q)
@@ -74,20 +66,191 @@ def singularity_spectrum(lag, mfdfa_data, q, lim = [None, None]):
 
     return hq, Dq
 
-def _tau(slopes, q):
+def scaling_exponents(lag: np.array, mfdfa: np.ndarray, q: np.array,
+                      lim: list=[None, None], interpolate: int=False):
     """
     Calculate the multifractal scaling exponents ``tau``.
+
+    Parameters
+    ----------
+    lag: np.array of ints
+        An array with the window sizes which where used in MFDFA.
+
+    mdfda: np.ndarray
+        Matrix of the fluctuation function from MFDFA
+
+    q: np.array
+        Fractal exponents used. Must be more than 2 points.
+
+    lim: list (default = [None, None])
+        List of lower and upper lag limits. If none, the polynomial fittings
+        include the full range of lag.
+
+    interpolate: int (default False)
+        Interpolates the ``q`` space to smoothed the singularity spectrum. Not
+        yet implemented.
+
+    Returns
+    -------
+    hq: np.array
+        Singularity strength ``hq``. The width of this function indicates the
+        strength of the multifractality. A width of ``max(hq) - min(hq) ≈ 0``
+        means the data is monofractal.
+
+    Dq: np.array
+        Singularity spectrum ``Dq``. The location of the maximum of ``Dq`` (with
+         ``hq`` as the abscissa) should be 1 and indicates the most prominent
+         exponent in the data.
+
+    Notes
+    -----
+    .. versionadded:: 0.4.1
+
+    References
+    ----------
+    .. [Kantelhardt2002] J. W. Kantelhardt, S. A. Zschiegner, E.
+        Koscielny-Bunde, S. Havlin, A. Bunde, H. E. Stanley. "Multifractal
+        detrended fluctuation analysis of nonstationary time series." Physica A,
+        316(1-4), 87–114, 2002.
     """
+
+    # Calculate the slopes
+    slopes = _slopes(lag, mfdfa, q, lim, interpolate)
+
     return ( q * slopes ) - 1
 
 def _hq(tau, q):
     """
     Calculate the singularity strength or Hölder exponents ``hq``.
+
+    Notes
+    -----
+    .. versionadded:: 0.4.1
     """
     return ( np.gradient(tau) / np.gradient(q) )
 
 def _Dq(tau, hq, q):
     """
     Calculate the singularity spectrum or fractal dimension ``Dq``.
+
+    Notes
+    -----
+    .. versionadded:: 0.4.1
     """
     return q * hq - tau
+
+def singularity_spectrum_plot(hq, Dq):
+    """
+    Plots the singularity spectrum.
+
+    Parameters
+    ----------
+    hq: np.array
+        Singularity strength ``hq`` as calculated with `singularity_spectrum`.
+
+    Dq: np.array
+        Singularity spectrum ``Dq`` as calculated with `singularity_spectrum`.
+
+    Returns
+    -------
+    fig: matplotlib fig
+        Returns the figure, useful if one wishes to use fig.savefig(...).
+
+    Notes
+    -----
+    .. versionadded:: 0.4.1
+    """
+
+    # Check if matplotlib is installed
+    _missing_library()
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(1,1)
+
+    ax.plot(hq, Dq, lw = 2, color = 'black')
+
+    ax.set_ylabel(r'Dq')
+    ax.set_xlabel(r'hq')
+
+    fig.tight_layout()
+
+    return fig
+
+def scaling_exponents_plot(tau, q):
+    """
+    Plots the scaling exponents, which is conventionally given with ``q`` in the
+    abscissa and ``tau`` in the ordinates.
+
+    Parameters
+    ----------
+    tau: np.array
+        Scaling exponents ``tau`` as calculated with `scaling_exponents`.
+
+    q: np.array
+        Singularity spectrum ``Dq`` as calculated with `singularity_spectrum`.
+
+    Returns
+    -------
+    fig: matplotlib fig
+        Returns the figure, useful if one wishes to use fig.savefig(...).
+
+    Notes
+    -----
+    .. versionadded:: 0.4.1
+
+    """
+
+    # Check if matplotlib is installed
+    _missing_library()
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(1,1)
+
+    ax.plot(tau, q, lw = 2, color = 'black')
+
+    ax.set_ylabel(r'tau')
+    ax.set_xlabel(r'q')
+
+    fig.tight_layout()
+
+    return fig
+
+def _slopes(lag: np.array, mfdfa: np.ndarray, q: np.array,
+            lim: list=[None, None], interpolate: int=False):
+    """
+    Extra the slopes of each q power obtained with MFDFA to later produce either
+    the singularity spectrum or the multifractal exponents.
+
+    Notes
+    -----
+    .. versionadded:: 0.4.1
+
+    """
+
+    # Fractal powers as floats
+    q = np.asarray_chkfinite(q, dtype = float)
+
+    # Ensure mfdfa has the same q-power entries as q
+    if mfdfa.shape[1] != q.shape[0]:
+        raise ValueError(
+            "Fluctuation function and q powers don't match in dimension.")
+
+    # Allocated array for slopes
+    slopes = np.zeros(len(q))
+
+    # Find slopes of each q-power
+    for i in range(len(q)):
+        slopes[i] = polyfit(np.log(lag[lim[0]:lim[1]]),
+                            np.log(mfdfa[lim[0]:lim[1],i]),
+                            1)[1]
+
+    return slopes
+
+def _missing_library():
+    try:
+        import matplotlib.pyplot as _plt
+    except ImportError:
+        raise ImportError(("matplotlib is required to do output the singularity"
+                           " spectrum plot. Please install matplotlib with 'pip"
+                           " install matplotlib' or another convinient way.")
+                         )
